976cedb012fbc91da018575332fe5c41835f2cb3
Didier MOISE <Didier.Moise@blackrock.com>
2020-11-09 12:03:12 +0100

    Handle API calls thru useEffect

--- src/components/SearchCat/SearchCatFormPF.jsx
+++ src/components/SearchCat/SearchCatFormPF.jsx
@@ -1,23 +1,52 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import Select from "react-select";
 
+import config from "../../config.json";
 
 import "./SearchCatForm.css";
 
 const imageTypes = ["jpg", "png", "gif"].map(v => ({ value: v, label: v }));
+const apiKey = config.apiKey;
+const baseUrl = "https://api.thecatapi.com/v1";
 
 function SearchCatForm({ excludedCats, addCats, limit }) {
     const loading = false;
-    const cats = [];
     const selectedCats = [];
 
     const [selectedCategory, setSelectedCategory] = useState();
     const [selectedImageTypes, setSelectedTimageTypes] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [cats, setCats] = useState([]);
+
+    // Fetch categories
+    useEffect(() => {
+        fetch(`${baseUrl}/categories`)
+            .then(response => response.json())
+            .then(categories =>
+                categories.map(({ name, id }) => ({ value: id, label: name }))
+            )
+            .then(setCategories);
+    }, [setCategories]);
+
+    // Fetch images at start and when category/images types are updated
+    useEffect(() => {
+        const imageTypes = selectedImageTypes.map(t => t.value);
+        const headers = new Headers();
+        headers.append("x-api-key", apiKey);
+        fetch(
+            `${baseUrl}/images/search?limit=${limit}&category_ids=${
+            selectedCategory ? selectedCategory.value : ""
+            }&mime_types=${imageTypes.join(",")}`
+        )
+            .then(response => response.json())
+            .then(setCats);
+    }, [setCats, selectedCategory, selectedImageTypes, limit]);
 
     return <div className="SearchCatForm">
         <Select
             placeholder="Category"
             className="SearchCatForm-select"
+            options={categories}
             value={selectedCategory}
             onChange={v => setSelectedCategory(v)}
         />
@@ -50,7 +79,7 @@ function SearchCatForm({ excludedCats, addCats, limit }) {
             )}
         {selectedCats.length > 0 && (
             <button
-                className="SearchCatForm-submit"                
+                className="SearchCatForm-submit"
             >
                 Add Cats
             </button>
